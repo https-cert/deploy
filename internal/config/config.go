c7d65793a206991c29b7e428e8e8129966a77cba
orange <3492989525@qq.com>
2026-01-01 01:09:58 +0800

    chore(config): 更新版本号至v0.5.0

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	Config   *Configuration
-	Version  = "v0.4.1"
+	Version  = "v0.5.0"
 	URL      = URLProd
 	URLProd  = "https://anssl.cn/deploy"
 	URLLocal = "http://localhost:9000/deploy"
