ce372539be06efe329f8d3fac14edd7cbef74993
orange <3492989525@qq.com>
2026-02-13 22:07:51 +0800

    chore(config): update version number to v0.5.4

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	Config   *Configuration
-	Version  = "v0.5.3"
+	Version  = "v0.5.4"
 	URL      = URLProd
 	URLProd  = "https://anssl.cn/deploy"
 	URLLocal = "http://localhost:9000/deploy"
